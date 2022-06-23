885422f5cea68d9719c27889a48b143985b1af06
Victor Leandro <83599460+Afrontoso@users.noreply.github.com>
2022-06-23 14:32:16 -0300

    Update da API

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const BASE_URL='https://thatcopy.pw/catapi/rest/'
+const BASE_URL='https://api.thecatapi.com/v1/images/search'
 const catBtn=document.getElementById('change-cat');
 const catImg=document.getElementById('cat');
 
